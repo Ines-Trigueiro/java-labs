f656e6a2197a91d37861a6e53250cdfd1f9bce87
Ines-Trigueiro <m.ines.trigueiro@gmail.com>
2026-09-12 22:33:03 +0200

    feat: add initial springboot project with gradle, openapi, postgress, apache kafka and docker compose

---
+++ backend/README.md
@@ -0,0 +1,10 @@
+# Springboot Rest API
+
+This is a simple Springboot Rest API application that demonstrates the use of Springboot framework to create RESTful web services. The application provides endpoints for basic CRUD operations, and it is built using Gradle as the build tool. The application also includes Docker Compose support for running a PostgreSQL database and Apache Kafka as dependencies.
+
+
+To build and run the application, you can use the following commands:
+```bash
+gradle build
+gradle bootRun
+```
---
+++ backend/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.1.1"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-kafka")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.1.0")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-kafka-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
